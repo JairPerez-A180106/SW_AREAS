c16f71537e17824a27c886aeb3ece090c5ba9140
Jair E. Pérez Mendoza <jeprez25@outlook.com>
2022-09-26 21:09:03 -0500

    Se agregaron las funciones al asmx

--- UNACH_Areas/UnachAreasGeoWS/AreasGeoWS.asmx.cs
+++ UNACH_Areas/UnachAreasGeoWS/AreasGeoWS.asmx.cs
@@ -1,13 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Web;
 using System.Web.Services;
 
 namespace UnachAreasGeoWS
 {
     /// <summary>
-    /// Descripción breve de AreasGeoWS
+    /// Este es un web service para entregar el cálculo de el área de figuras geométricas
     /// </summary>
     [WebService(Namespace = "http://tempuri.org/")]
     [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
@@ -21,6 +22,31 @@ namespace UnachAreasGeoWS
         public string HelloWorld()
         {
             return "Hola a todos";
+        }
+
+        [WebMethod]
+        public decimal cuadrado(decimal LADO)
+        {
+            CalqAreas A = new CalqAreas();
+            A.clado = LADO;
+            return A.cuadrado();
+        }
+
+        [WebMethod]
+        public decimal triangulo(decimal BASE, decimal ALTURA)
+        {
+            CalqAreas A = new CalqAreas();
+            A.tbase = BASE;
+            A.taltura = ALTURA;
+            return A.triangulo();
+        }
+
+        [WebMethod]
+        public decimal circulo(decimal RADIO)
+        {
+            CalqAreas A = new CalqAreas();
+            A.cradio = RADIO;
+            return A.circulo();
         }
     }
 }
---
+++ UNACH_Areas/UnachAreasGeoWS/CalqAreas.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UnachAreasGeoWS
+{
+    public class CalqAreas
+    {
+        public decimal clado { get; set; }
+        public decimal tbase { get; set; }
+        public decimal taltura { get; set; }
+        public decimal cradio { get; set; }
+
+
+        public decimal cuadrado()
+        {
+            return (clado * clado);
+        }
+
+        public decimal triangulo()
+        {
+            return ((tbase * taltura) / 2);
+        }
+
+        public decimal circulo()
+        {
+            decimal pi = Convert.ToDecimal(Math.PI);
+            return (pi * (cradio * cradio));
+        }
+    }
+}
